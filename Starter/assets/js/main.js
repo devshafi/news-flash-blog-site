0b897c1bd48a48d1daa70f78df8469293c5c8766
Fozle Rabbi Shafi <frshafi49@gmail.com>
2022-05-18 03:25:21 +0600

    open mobile menu onclick nav

--- Starter/assets/js/main.js
+++ Starter/assets/js/main.js
@@ -1,6 +1,35 @@
 // Grab elements
 
+const selectElement = selector => {
+    const element = document.querySelector(selector);
+    if (element) return element
+
+    throw new Error(
+        `Something went wrong, make sure that ${selector} exists or is typed correctly`
+    )
+}
+
 //Nav styles on scroll
+const scrollHeader = () => {
+    const headerElement = selectElement('.header');
+    const HEADER_SCROLL_START_THRESHOLD = 15;
+    if (this.scrollY > HEADER_SCROLL_START_THRESHOLD) {
+        headerElement.classList.add('activated')
+    } else[
+        headerElement.classList.remove('activated')
+    ]
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+}
+menuToggleIcon.addEventListener('click', toggleMenu);
+
 
 // Open menu & search pop-up
 
